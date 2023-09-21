76f49caeae2b2d93473555b502b454488ebf1f92
Morgan <101351216+devjoon@users.noreply.github.com>
2023-09-21 13:37:01 +0900

    [I Am Poor] App complete

---
+++ I AM Poor/I AM Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM Poor
+//
+//  Created by hyunMac on 2023/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
